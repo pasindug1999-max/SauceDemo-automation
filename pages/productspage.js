8c9c3fddfef7c28835462e281c5d2fb29e71ba53
Pasindu G <pasindug199@gmail.com>
2026-09-03 12:28:46 +0530

    Added modifications to Products class

--- pages/productspage.js
+++ pages/productspage.js
@@ -4,20 +4,13 @@ class Products {
 
         this.page = page;
         this.dropdown = page.locator("//select[@class='product_sort_container']");
-        this.pricelow = page.getByLabel("Price (low to high)");
-        this.pricehight = page.getByLabel("Price (high to low)");
-        this.nameascending = page.locator("//option[@value='az']");
-        this.namedescending = page.locator("//option[@value='za']");
+        
 
     }
 
-    async selectdropdown(){
+    async selectdropdown(option) {
 
-        await this.dropdown.click();
-        await this.pricelow.click();
-        await this.pricehight.click();
-        await this.nameascending.click();
-        await this.namedescending.click();
+        await this.dropdown.selectOption(option);
 
 
     }
