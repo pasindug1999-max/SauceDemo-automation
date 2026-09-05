8d197327b80dc65eb2b0a49d122f75f8d093147b
Pasindu G <pasindug199@gmail.com>
2026-09-05 07:36:58 +0530

    Created tests for adding multiple products and created assertions

--- tests/addtocart.spec.js
+++ tests/addtocart.spec.js
@@ -6,19 +6,54 @@ import Cart from "../pages/addtocartpage";
 const user = "standard_user";
 const password = "secret_sauce";
 
-test("Add to Cart", async ({page})=>{
 
-    const Loginpage = new Login(page);
-    const cartpage = new Cart(page);
+test.beforeEach(async ({page})=>{
+
+    const loginpage = new Login(page);
 
     await page.goto("https://www.saucedemo.com/");
 
+    await loginpage.loginToApplication(user,password);
 
-    await Loginpage.loginToApplication(user,password);
+});
+
+
+// Add a single product to cart
+
+test("Add a single product to cart", async function({page}){
+
+    const cartpage = new Cart(page);
 
     await cartpage.addToCart();
 
-    await page.waitForTimeout(3000);
+    await expect(page.locator("//span[@class='shopping_cart_badge']")).toHaveCount(1);
+
+    //await page.waitForTimeout(3000);
+
+});
+
+// Remove a product from cart
+
+test("Remove a product added to cart", async function({page}){
+
+    const cartpage = new Cart(page);
+
+    await cartpage.addToCart();
+
+    await cartpage.removeFromCart();
+
+    //await  page.waitForTimeout(3000);
+
+    await expect(page.locator("//span[@class='shopping_cart_badge']")).toHaveCount(0);
+});
 
 
-})
+test("Add multiple products to cart", async function({page}){
+
+    const cartpage = new Cart(page);
+
+    await cartpage.addMultipleProducts();
+
+    await expect(page.locator("//span[@class='shopping_cart_badge']")).toHaveText("3");
+
+});
