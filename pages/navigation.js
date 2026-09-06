962f195e8acbf0721d650b3ef12217358a233ba0
Pasindu G <pasindug199@gmail.com>
2026-09-06 13:39:41 +0530

    Modified Navigation class

--- pages/navigation.js
+++ pages/navigation.js
@@ -4,15 +4,22 @@ class Navigation {
 
         this.page = page;
         this.menu = page.getByRole("Button", {name:"Open Menu"});
+        this.allItems = page.getByRole("link", {name: "All Items"});
         this.logout = page.locator("(//a[normalize-space()='Logout'])[1]");
         this.about = page.getByRole("link", {name:"About"});
         this.resetApp = page.getByRole("link",{name: "Reset App State"});
+        this.closeMenu = page.geyByRole("button", {name: "Close Menu"});
         
     }
 
     async openMenu(){
 
         await this.menu.click();
+    }
+
+    async cliclAllItems(){
+
+        await this.allItems.click();
     }
 
     async logOutFromApp(){
@@ -29,8 +36,12 @@ class Navigation {
 
         await this.resetApp.click();
     }
-    
 
+    async closesideMenu(){
+
+        await this.closeMenu.click();
+    }
+    
 
 
 }
